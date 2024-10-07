0ddc80e60caa18662bb1a1b58f4a968321ac100e
squareCaaat <danny79052@gmail.com>
2024-10-08 02:37:32 +0900

    feat: MemberRoleRepository

---
+++ src/main/java/com/wap/cano_be/member/repository/MemberRoleRepository.java
@@ -0,0 +1,7 @@
+package com.wap.cano_be.member.repository;
+
+import com.wap.cano_be.member.entity.MemberRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRoleRepository extends JpaRepository<MemberRole, Long> {
+}
