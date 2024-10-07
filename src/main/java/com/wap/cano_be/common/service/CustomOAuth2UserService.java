6446f7876cc596e87cc90861ce31f17fdaa6a3a3
squareCaaat <danny79052@gmail.com>
2024-10-08 02:15:26 +0900

    feat: CustomOAuth2UserService loadUser

---
+++ src/main/java/com/wap/cano_be/common/service/CustomOAuth2UserService.java
@@ -0,0 +1,44 @@
+package com.wap.cano_be.common.service;
+
+import com.wap.cano_be.member.entity.Member;
+import com.wap.cano_be.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 1. 유저 정보 가져오기
+        Map<String, Object> oAuth2UserAttributes = super.loadUser(userRequest).getAttributes();
+
+        // 2. registrationId 가져오기 (third-party id)
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        // 3. userNameAttributeName 가져오기
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        // 4. 유저 정보 DTO 생성
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2UserAttributes);
+
+        // 5. 회원가입 및 로그인
+        Member member = getOrSave(oAuth2UserInfo);
+
+        // 6. OAuth2User 반환
+        return new PrincipalDetails(member, oAuth2UserAttributes, userNameAttributeName);
+    }
+
+
+}
