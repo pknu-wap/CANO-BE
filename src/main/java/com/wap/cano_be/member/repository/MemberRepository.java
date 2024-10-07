c7753fab3e6759738d1582394f77a7bed4e06a31
squareCaaat <danny79052@gmail.com>
2024-10-08 00:36:09 +0900

    chore: type change

--- src/main/java/com/wap/cano_be/member/repository/MemberRepository.java
+++ src/main/java/com/wap/cano_be/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.wap.cano_be.member.repository;
 import com.wap.cano_be.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Member findByLoginId(String loginId);
+    Optional<Member> findByLoginId(String loginId);
 }
