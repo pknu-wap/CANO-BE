b744727aaa9f85b5466068ba5ee87e09f29c2c40
squareCaaat <danny79052@gmail.com>
2024-10-08 02:24:50 +0900

    feat: ofGoogle

--- src/main/java/com/wap/cano_be/common/dto/OAuth2UserInfo.java
+++ src/main/java/com/wap/cano_be/common/dto/OAuth2UserInfo.java
@@ -19,5 +19,25 @@ public record OAuth2UserInfo(
         };
     }
 
+    private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .profile((String) attributes.get("picture"))
+                .build();
+    }
+
+    private static OAuth2UserInfo ofKakao(Map<String, Object> attributes) {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        return OAuth2UserInfo.builder()
+                .name((String) profile.get("nickname"))
+                .email((String) account.get("email"))
+                .profile((String) profile.get("profile_image_url"))
+                .build();
+    }
+
+
 
 }
