a21533360da266e198011974dd7079bd1b0f0b13
squareCaaat <danny79052@gmail.com>
2024-10-08 00:51:10 +0900

    feat: createUserDetails

--- src/main/java/com/wap/cano_be/common/service/CustomUserDetailsService.java
+++ src/main/java/com/wap/cano_be/common/service/CustomUserDetailsService.java
@@ -1,12 +1,17 @@
 package com.wap.cano_be.common.service;
 
+import com.wap.cano_be.common.dto.CustomUser;
+import com.wap.cano_be.member.entity.Member;
 import com.wap.cano_be.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -25,6 +30,17 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
 
+    }
+
+    private UserDetails createUserDetails(Member member){
+        return new CustomUser(
+                member.getId(),
+                member.getLoginId(),
+                passwordEncoder.encode(member.getPassword()
+                ), member.getMemberRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                        .collect(Collectors.toList())
+        );
     }
 
 }
