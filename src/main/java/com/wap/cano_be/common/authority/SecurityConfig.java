0af39d039a94cfc4f2620cba9eba42f946e68a7d
squareCaaat <danny79052@gmail.com>
2024-10-07 23:23:48 +0900

    feat: passwordEncoder

--- src/main/java/com/wap/cano_be/common/authority/SecurityConfig.java
+++ src/main/java/com/wap/cano_be/common/authority/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,5 +31,10 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtTokenProvider, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
