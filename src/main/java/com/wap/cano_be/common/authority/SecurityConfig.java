31d7d33d25e525cc8e130ba06b737efdee6cc6c9
squareCaaat <danny79052@gmail.com>
2024-10-07 23:20:41 +0900

    feat: SecurityConfig filterChain

---
+++ src/main/java/com/wap/cano_be/common/authority/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.wap.cano_be.common.authority;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HttpSession httpSession) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(httpRequest -> httpRequest
+                        .requestMatchers("/api/member/signup", "/api/member/login").anonymous()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtTokenProvider, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
