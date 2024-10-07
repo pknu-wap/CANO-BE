7b6beae4c1fe059c8840ce61394d71ae141bd3ef
squareCaaat <danny79052@gmail.com>
2024-10-08 02:21:11 +0900

    feat: OAuth2UserInfo of

---
+++ src/main/java/com/wap/cano_be/common/dto/OAuth2UserInfo.java
@@ -0,0 +1,23 @@
+package com.wap.cano_be.common.dto;
+
+import jakarta.security.auth.message.AuthException;
+import lombok.Builder;
+
+import java.util.Map;
+
+@Builder
+public record OAuth2UserInfo(
+        String name,
+        String email,
+        String profile
+) {
+    public static OAuth2UserInfo of(String registrationId, Map<String, Object> attributes) {
+        return switch (registrationId) {
+            case "google" -> ofGoogle(attributes);
+            case "kakao" -> ofKakao(attributes);
+            default -> throw new AuthException(ILLEGAL_REGISTRATION_ID);
+        };
+    }
+
+
+}
