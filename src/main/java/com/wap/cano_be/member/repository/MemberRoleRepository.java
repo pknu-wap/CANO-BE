bba1705dbd28a81aae967ab768fa5c7617bdc0d5
squareCaaat <danny79052@gmail.com>
2024-10-09 03:06:48 +0900

    chore: unused MemberRole delete

--- src/main/java/com/wap/cano_be/member/repository/MemberRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wap.cano_be.member.repository;
-
-import com.wap.cano_be.member.entity.MemberRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRoleRepository extends JpaRepository<MemberRole, Long> {
-}
