dae24b5eba24b32ba431641f808c50f7eb8b3a75
squareCaaat <danny79052@gmail.com>
2024-10-10 21:22:20 +0900

    feat: add ResponseCode IS_DUPLICATED

--- src/main/java/com/wap/cano_be/common/ResponseCode.java
+++ src/main/java/com/wap/cano_be/common/ResponseCode.java
@@ -5,7 +5,8 @@ public enum ResponseCode {
     DATABASE_ERROR("데이터베이스 오류가 발생했습니다."),
     VALIDATION_FAILED("유효성 검사에 실패했습니다."),
     NO_SUCH_USER("사용자를 찾지 못했습니다."),
-    CERTIFICATION_FAIL("권한 인증에 실패했습니다.");
+    CERTIFICATION_FAIL("권한 인증에 실패했습니다."),
+    IS_DUPLICATED("중복 데이터입니다.");
 
     private final String responseMessage;
 
