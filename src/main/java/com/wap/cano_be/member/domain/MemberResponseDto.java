dbc43271886ee984731f805ef33e08f6d4a3fad0
squareCaaat <danny79052@gmail.com>
2024-10-10 21:21:54 +0900

    feat: return ResponseDto if member is duplicated

--- src/main/java/com/wap/cano_be/member/domain/MemberResponseDto.java
+++ src/main/java/com/wap/cano_be/member/domain/MemberResponseDto.java
@@ -1,8 +1,11 @@
 package com.wap.cano_be.member.domain;
 
+import com.wap.cano_be.common.ResponseCode;
 import com.wap.cano_be.common.ResponseDto;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 public class MemberResponseDto extends ResponseDto {
@@ -18,6 +21,11 @@ public class MemberResponseDto extends ResponseDto {
         this.email = email;
         this.socialId = socialId;
         this.profileImageUrl = profileImageUrl;
+    }
+
+    public static ResponseEntity<ResponseDto> duplicatedMember(){
+        ResponseDto responseDto = new ResponseDto(ResponseCode.IS_DUPLICATED.name(), "이미 존재하는 유저입니다.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
     }
 
 }
