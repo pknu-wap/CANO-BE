b766427069fa4142a63534e8070ca3340f844259
squareCaaat <danny79052@gmail.com>
2024-10-12 18:25:07 +0900

    feat: MemberController - add login

--- src/main/java/com/wap/cano_be/member/controller/MemberController.java
+++ src/main/java/com/wap/cano_be/member/controller/MemberController.java
@@ -1,28 +1,33 @@
 package com.wap.cano_be.member.controller;
 
 import com.wap.cano_be.common.ResponseDto;
-import com.wap.cano_be.member.domain.Member;
-import com.wap.cano_be.member.domain.MemberRequestDto;
-import com.wap.cano_be.member.domain.MemberResponseDto;
-import com.wap.cano_be.member.domain.PrincipalDetail;
+import com.wap.cano_be.member.domain.*;
+import com.wap.cano_be.member.dto.MemberRequestDto;
+import com.wap.cano_be.member.dto.MemberResponseDto;
+import com.wap.cano_be.oauth2.dto.OAuth2LoginDto;
 import com.wap.cano_be.member.service.MemberService;
-import lombok.RequiredArgsConstructor;
+import com.wap.cano_be.oauth2.service.OAuth2LoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;
+    private final OAuth2LoginService oAuth2LoginService;
+
+    @Autowired
+    public MemberController(MemberService memberService, OAuth2LoginService oAuth2LoginService){
+        this.memberService = memberService;
+        this.oAuth2LoginService = oAuth2LoginService;
+    }
 
     @PostMapping("/signUp")
     public ResponseEntity<ResponseDto> signUp(@RequestBody MemberRequestDto memberRequestDto) {
@@ -36,6 +41,17 @@ public class MemberController {
             memberService.saveMember(memberRequestDto);
         }
         return ResponseEntity.ok().body(new ResponseDto());
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<ResponseDto> login(@RequestBody OAuth2LoginDto oAuth2LoginDto) {
+        switch (oAuth2LoginDto.provider()){
+            case "kakao":
+                return oAuth2LoginService.oauth2Login(oAuth2LoginDto);
+            default:
+                return ResponseEntity.badRequest().body(new ResponseDto("UN_SUPPORT", "지원하지 않는 Provider 입니다."));
+        }
+
     }
 
     @GetMapping("/user")
