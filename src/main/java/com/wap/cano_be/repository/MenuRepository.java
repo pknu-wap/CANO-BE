68ba9cec2cbccc36dcb80cd8dd0bc8d19a4ed591
squareCaaat <danny79052@gmail.com>
2024-11-08 18:39:21 +0900

    feat: add Menu Repository

---
+++ src/main/java/com/wap/cano_be/repository/MenuRepository.java
@@ -0,0 +1,6 @@
+package com.wap.cano_be.repository;
+
+import com.wap.cano_be.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {}
