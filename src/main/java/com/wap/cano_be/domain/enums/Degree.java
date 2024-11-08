0e91abb979d6d53b25c9ba44abb8a2fec94b80aa
squareCaaat <danny79052@gmail.com>
2024-11-08 20:58:03 +0900

    feat: mod Degree Enum has percentage field

--- src/main/java/com/wap/cano_be/domain/enums/Degree.java
+++ src/main/java/com/wap/cano_be/domain/enums/Degree.java
@@ -1,9 +1,12 @@
 package com.wap.cano_be.domain.enums;
 
 public enum Degree {
-    NONE,
-    LOW,
-    MEDIUM,
-    HIGH,
-    VERY_HIGH
+    NONE(0),
+    LOW(0.2),
+    MEDIUM(0.4),
+    HIGH(0.6),
+    VERY_HIGH(0.8);
+
+    Degree(double percentage) {
+    }
 }
