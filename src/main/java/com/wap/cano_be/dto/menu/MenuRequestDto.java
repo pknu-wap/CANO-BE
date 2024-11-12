d56bf402470a3575d254e9788edbdd490a2d9a2f
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-11-12 20:41:41 +0900

    fix: remove unused fields

--- src/main/java/com/wap/cano_be/dto/menu/MenuRequestDto.java
+++ src/main/java/com/wap/cano_be/dto/menu/MenuRequestDto.java
@@ -1,14 +1,9 @@
 package com.wap.cano_be.dto.menu;
 
-import java.util.List;
-
 public record MenuRequestDto(
+        String cafeName,
         String name,
         int price,
-        String acidity,
-        String body,
-        String bitterness,
-        String sweetness,
-        List<String> aromas
+        String imageUrl
 ) {
 }
