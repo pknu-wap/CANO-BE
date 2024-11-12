3038eed2f1f8a9fad32a30bb16252bf9617c4f29
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-11-12 20:42:53 +0900

    feat: add percentage field

--- src/main/java/com/wap/cano_be/domain/enums/Degree.java
+++ src/main/java/com/wap/cano_be/domain/enums/Degree.java
@@ -1,5 +1,8 @@
 package com.wap.cano_be.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Degree {
     NONE(0),
     LOW(0.2),
@@ -7,6 +10,9 @@ public enum Degree {
     HIGH(0.6),
     VERY_HIGH(0.8);
 
+    private double percentage;
+
     Degree(double percentage) {
+        this.percentage = percentage;
     }
 }
