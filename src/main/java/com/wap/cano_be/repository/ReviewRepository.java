f678d8ad97f0b2bd757094247565f87237495129
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-11-15 00:02:26 +0900

    feat: add Review CRD logics

--- src/main/java/com/wap/cano_be/repository/ReviewRepository.java
+++ src/main/java/com/wap/cano_be/repository/ReviewRepository.java
@@ -1,8 +1,13 @@
 package com.wap.cano_be.repository;
 
+import com.wap.cano_be.domain.Member;
+import com.wap.cano_be.domain.Menu;
 import com.wap.cano_be.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+import java.util.List;
 
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllByMenu(Menu menu);
+    void deleteByMemberAndMenu(Member member, Menu menu);
 }
