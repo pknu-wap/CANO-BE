a46e402ec5c8b28669b8ed5d010f016aeda30b63
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-11-15 00:01:06 +0900

    feat: add attributes fields

--- src/main/java/com/wap/cano_be/domain/Review.java
+++ src/main/java/com/wap/cano_be/domain/Review.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Review {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,6 +21,15 @@ public class Review {
     @Lob
     private String contents;
     private Double score;
+    private Double acidity;
+    private Double body;
+    private Double bitterness;
+    private Double sweetness;
+
+    @ElementCollection
+    @CollectionTable(name = "review_aromas", joinColumns = @JoinColumn(name = "review_id"))
+    @Column(name = "aromas")
+    private List<String> aromas = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "member_id")
@@ -41,12 +52,19 @@ public class Review {
     public Review(){}
 
     @Builder
-    public Review(String contents, Double score, Member member, Menu menu, List<String> imageUrls) {
+    public Review(String contents, Double score, Double acidity, Double body, Double bitterness, Double sweetness, Member member, Menu menu, List<String> imageUrls, List<String> aromas, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.contents = contents;
         this.score = score;
+        this.acidity = acidity;
+        this.body = body;
+        this.bitterness = bitterness;
+        this.sweetness = sweetness;
         this.member = member;
         this.menu = menu;
         this.imageUrls = imageUrls;
+        this.aromas = aromas;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
 }
